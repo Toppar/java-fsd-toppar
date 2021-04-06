68cf9468cccda837347f000f0a91c2e2b7f428d9
Toppar <toppar_bourmaf@dell.com>
2021-04-06 13:07:39 -0500

    Create MainController.java

---
+++ SpringStarter/SpringStarter/src/main/java/com/example/SpringStarter/MainController.java
@@ -0,0 +1,18 @@
+package com.example.SpringStarter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public void printHello()
+	{
+		return("Hello!");
+	}
+		
+		
+		
+}
