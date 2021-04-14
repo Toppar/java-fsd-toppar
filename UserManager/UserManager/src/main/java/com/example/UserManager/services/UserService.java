125d1f7dcb7554708a97fb7c6aa9a862fc43e15b
Toppar <toppar_bourmaf@dell.com>
2021-04-13 21:35:47 -0500

    initial commit

---
+++ UserManager/UserManager/src/main/java/com/example/UserManager/services/UserService.java
@@ -0,0 +1,34 @@
+package com.example.UserManager.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.UserManager.entities.User;
+import com.example.UserManager.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public Iterable<User> GetAllUsers() {
+		return userRepository.findAll();
+	}
+
+	public User GetUserById(int id) {
+
+		// TODO: Handle non-existent user. Currently throws an exception
+		Optional<User> user = userRepository.findById(id);
+		return user.get();
+
+	}
+
+	public User EditUser(User user) {
+
+		// TODO: Not doing any error checking
+
+		return userRepository.save(user);
+	}
+}
