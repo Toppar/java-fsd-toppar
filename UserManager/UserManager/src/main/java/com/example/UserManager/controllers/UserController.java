be6bc90f81cc9b87f6afc9d4bac59d34f5c781b4
Toppar <toppar_bourmaf@dell.com>
2021-04-15 15:32:16 -0500

    Initial commit

--- UserManager/UserManager/src/main/java/com/example/UserManager/controllers/UserController.java
+++ UserManager/UserManager/src/main/java/com/example/UserManager/controllers/UserController.java
@@ -2,7 +2,6 @@ package com.example.UserManager.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,20 +16,55 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping(value = "/userquery", method = RequestMethod.GET)
-	public String showUsers() {
-		return ("userquery");
-	}
+	  @RequestMapping(value="/userquery", method = RequestMethod.GET)
+	    public String showUsers(ModelMap model)
+	    {
+	    	model.addAttribute("message", "Welcome!");
+	    	return("userquery");
+	    }
+	    @RequestMapping(value="/userquery", method = RequestMethod.POST)
+	    public String handleQuery(
+	    		@RequestParam(value = "uid", required = true) Integer uid,
+	    		ModelMap model) {
+	    
+	    	
+	    	try {
+	    	//TODO: Handle empty or invalid UserId
+	    		User user = userService.GetUserById(uid);
+	    	
+	    		model.addAttribute("user", user);
+	    
+	    		return("useredit");
+	    	}
+	    	catch(Exception ex) {
+	    		model.addAttribute("message", "ERROR: Unknown userid");
+	    		return("userquery");
+	    	}
+	    }
+	    
+	    
+	    @RequestMapping(value="/useredit", method = RequestMethod.POST)
+	    public String handleEdit(
+	    		@RequestParam(value = "uid", required = true) Integer uid,
+	    		@RequestParam(value = "email", required = true) String email,
+	    		@RequestParam(value = "username", required = true) String username,
+	    		@RequestParam(value = "password", required = true) String password,
+	    		ModelMap model) {
+	    	//TODO: Handle empty or invalid UserId
+	    	try {
+	    		User user = userService.GetUserById(uid);
+	    	    
+	    		user.setEmail(email);
+	    		user.setName(username);
+	    		user.setPassword(password);
+	    	
+	    		userService.EditUser(user);
+	    		model.addAttribute("message", "User Edited Successfully");
+	    	}
+	    	catch (Exception ex) {
+	        	model.addAttribute("message", "User Edit NOT Successful");
 
-	@RequestMapping(value = "/userquery", method = RequestMethod.POST)
-	public String handleQuery(@RequestParam(value = "uid", required = true) Integer uid, Model model) {
-
-		// TODO: Handle empty or invalid UserId
-		User user = userService.GetUserById(uid);
-
-		model.addAttribute("user", user);
-
-		return ("useredit");
-	}
-
+	    	}
+	    	return("userquery");
+	    }
 }
--- UserManager/UserManager/src/main/webapp/WEB-INF/jsp/userquery.jsp
+++ UserManager/UserManager/src/main/webapp/WEB-INF/jsp/userquery.jsp
@@ -1,5 +1,7 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix ="c" %>
 
+<p>${message}</p>
+
 <html>
 <head>
 <style>
